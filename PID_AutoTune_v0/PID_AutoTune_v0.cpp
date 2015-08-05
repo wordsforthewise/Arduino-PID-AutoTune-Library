991b088abcb970b0b30ccc34637779e0df97bf94
wordsforthewise <wordsforthewise@gmail.com>
2015-08-05 10:16:57 -0700

    convert tabs to spaces -- best practices

--- PID_AutoTune_v0/PID_AutoTune_v0.cpp
+++ PID_AutoTune_v0/PID_AutoTune_v0.cpp
@@ -9,63 +9,63 @@
 
 PID_ATune::PID_ATune(double* Input, double* Output)
 {
-	input = Input;
-	output = Output;
-	controlType =0 ; //default to PI
-	noiseBand = 0.5;
-	running = false;
-	oStep = 30;
-	SetLookbackSec(10);
-	lastTime = millis();
-	
+    input = Input;
+    output = Output;
+    controlType =0 ; //default to PI
+    noiseBand = 0.5;
+    running = false;
+    oStep = 30;
+    SetLookbackSec(10);
+    lastTime = millis();
+    
 }
 
 
 
 void PID_ATune::Cancel()
 {
-	running = false;
+    running = false;
 } 
  
 int PID_ATune::Runtime()
 {
-	justevaled=false;
-	if(peakCount>9 && running)
-	{
-		running = false;
-		FinishUp();
-		return 1;
-	}
-	unsigned long now = millis();
-	
-	if((now-lastTime)<sampleTime) return false;
-	lastTime = now;
-	double refVal = *input;
-	justevaled=true;
-	if(!running)
-	{ //initialize working variables the first time around
-		peakType = 0;
-		peakCount=0;
-		justchanged=false;
-		absMax=refVal;
-		absMin=refVal;
-		setpoint = refVal;
-		running = true;
-		outputStart = *output;
-		*output = outputStart+oStep;
-	}
-	else
-	{
-		if(refVal>absMax)absMax=refVal;
-		if(refVal<absMin)absMin=refVal;
-	}
-	
-	//oscillate the output base on the input's relation to the setpoint
-	
-	if(refVal>setpoint+noiseBand) *output = outputStart-oStep;
-	else if (refVal<setpoint-noiseBand) *output = outputStart+oStep;
-	
-	
+    justevaled=false;
+    if(peakCount>9 && running)
+    {
+        running = false;
+        FinishUp();
+        return 1;
+    }
+    unsigned long now = millis();
+    
+    if((now-lastTime)<sampleTime) return false;
+    lastTime = now;
+    double refVal = *input;
+    justevaled=true;
+    if(!running)
+    { //initialize working variables the first time around
+        peakType = 0;
+        peakCount=0;
+        justchanged=false;
+        absMax=refVal;
+        absMin=refVal;
+        setpoint = refVal;
+        running = true;
+        outputStart = *output;
+        *output = outputStart+oStep;
+    }
+    else
+    {
+        if(refVal>absMax)absMax=refVal;
+        if(refVal<absMin)absMin=refVal;
+    }
+    
+    //oscillate the output base on the input's relation to the setpoint
+    
+    if(refVal>setpoint+noiseBand) *output = outputStart-oStep;
+    else if (refVal<setpoint-noiseBand) *output = outputStart+oStep;
+    
+    
   //bool isMax=true, isMin=true;
   isMax=true;isMin=true;
   //id peaks
@@ -79,8 +79,8 @@ int PID_ATune::Runtime()
   lastInputs[0] = refVal;  
   if(nLookBack<9)
   {  //we don't want to trust the maxes or mins until the inputs array has been filled
-	return 0;
-	}
+    return 0;
+    }
   
   if(isMax)
   {
@@ -113,18 +113,18 @@ int PID_ATune::Runtime()
     double avgSeparation = (abs(peaks[peakCount-1]-peaks[peakCount-2])+abs(peaks[peakCount-2]-peaks[peakCount-3]))/2;
     if( avgSeparation < 0.05*(absMax-absMin))
     {
-		FinishUp();
+        FinishUp();
       running = false;
-	  return 1;
-	 
+      return 1;
+     
     }
   }
    justchanged=false;
-	return 0;
+    return 0;
 }
 void PID_ATune::FinishUp()
 {
-	  *output = outputStart;
+      *output = outputStart;
       //we can generate tuning parameters!
       Ku = 4*(2*oStep)/((absMax-absMin)*3.14159);
       Pu = (double)(peak1-peak2) / 1000;
@@ -132,65 +132,65 @@ void PID_ATune::FinishUp()
 
 double PID_ATune::GetKp()
 {
-	return controlType==1 ? 0.6 * Ku : 0.4 * Ku;
+    return controlType==1 ? 0.6 * Ku : 0.4 * Ku;
 }
 
 double PID_ATune::GetKi()
 {
-	return controlType==1? 1.2*Ku / Pu : 0.48 * Ku / Pu;  // Ki = Kc/Ti
+    return controlType==1? 1.2*Ku / Pu : 0.48 * Ku / Pu;  // Ki = Kc/Ti
 }
 
 double PID_ATune::GetKd()
 {
-	return controlType==1? 0.075 * Ku * Pu : 0;  //Kd = Kc * Td
+    return controlType==1? 0.075 * Ku * Pu : 0;  //Kd = Kc * Td
 }
 
 void PID_ATune::SetOutputStep(double Step)
 {
-	oStep = Step;
+    oStep = Step;
 }
 
 double PID_ATune::GetOutputStep()
 {
-	return oStep;
+    return oStep;
 }
 
 void PID_ATune::SetControlType(int Type) //0=PI, 1=PID
 {
-	controlType = Type;
+    controlType = Type;
 }
 int PID_ATune::GetControlType()
 {
-	return controlType;
+    return controlType;
 }
-	
+    
 void PID_ATune::SetNoiseBand(double Band)
 {
-	noiseBand = Band;
+    noiseBand = Band;
 }
 
 double PID_ATune::GetNoiseBand()
 {
-	return noiseBand;
+    return noiseBand;
 }
 
 void PID_ATune::SetLookbackSec(int value)
 {
     if (value<1) value = 1;
-	
-	if(value<25)
-	{
-		nLookBack = value * 4;
-		sampleTime = 250;
-	}
-	else
-	{
-		nLookBack = 100;
-		sampleTime = value*10;
-	}
+    
+    if(value<25)
+    {
+        nLookBack = value * 4;
+        sampleTime = 250;
+    }
+    else
+    {
+        nLookBack = 100;
+        sampleTime = value*10;
+    }
 }
 
 int PID_ATune::GetLookbackSec()
 {
-	return nLookBack * sampleTime / 1000;
+    return nLookBack * sampleTime / 1000;
 }
